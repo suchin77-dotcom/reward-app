5edb0e4128172e66a5e8fd8aa6b952a68d4b9a3b
suchin77-dotcom <suchin7777@gmail.com>
2026-05-11 00:20:48 +0900

    Update client.ts

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,7 +1,7 @@
-import { createClient as createBrowserClient } from '@supabase/supabase-js';
+import { createClient as createSupabaseClient } from '@supabase/supabase-js';
 
 export const createClient = () =>
-  createBrowserClient(
+  createSupabaseClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
